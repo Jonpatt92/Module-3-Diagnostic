e853f1fa8e9a2e2afeeee55c7055e6afbbcbdf71
Jon Patterson <JonPatt92@gmail.com>
2019-12-09 10:06:19 -0700

    Build potter_service to connect to api endpoint

--- app/services/potter_service.rb
+++ app/services/potter_service.rb
@@ -1,3 +1,23 @@
 class PotterService
+  def get_house_characters(house)
+      response = fetch_data("/v1/characters?house=#{house}")
+      parse_data(response)[:results]
+  end
 
+  private
+
+  def parse_data(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def fetch_data(url)
+    connection.get(url)
+  end
+
+  def connection
+    Faraday.new(url: "https://www.potterapi.com") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV['POTTER_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 end
