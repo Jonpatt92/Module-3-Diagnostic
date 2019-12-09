111b670ac40f26b66661530dd2d337a375b3926f
Jon Patterson <JonPatt92@gmail.com>
2019-12-09 10:47:27 -0700

    Build search_facade

--- app/facades/potter_search_facade.rb
+++ app/facades/potter_search_facade.rb
@@ -2,4 +2,30 @@ class PotterSearchFacade
   def initialize(house)
     @house = house
   end
+
+  def characters
+    @raw_character_data ||= get_characters
+    create_characters(@raw_character_data)
+  end
+
+  def get_characters
+    @data ||= service.get_house_characters(house)
+  end
+
+  def character_count
+    characters.length
+  end
+
+  private
+  attr_reader :house
+
+  def service
+    PotterService.new
+  end
+
+  def create_characters(raw_character_data)
+    raw_character_data.map do |character|
+      Character.new(character)
+    end
+  end
 end
